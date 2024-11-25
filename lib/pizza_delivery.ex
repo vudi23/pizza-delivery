3b1d0dd083b55199996261955f5bd0ce68381603
vudic <56545508+vudi23@users.noreply.github.com>
2024-11-25 18:41:51 +0100

    Implement find late pizzas

--- lib/pizza_delivery.ex
+++ lib/pizza_delivery.ex
@@ -1,18 +1,17 @@
 defmodule PizzaDelivery do
-  @moduledoc """
-  Documentation for `PizzaDelivery`.
-  """
+  def find_late_pizzas(orders) do
+    Enum.reduce(orders, [], fn order, late_orders ->
+      delivery_time = calculate_delivery_time(order)
 
-  @doc """
-  Hello world.
+      if delivery_time > 20,
+        do: [Map.put(order, :minutes_late, delivery_time - 20) | late_orders],
+        else: late_orders
+    end)
+  end
 
-  ## Examples
-
-      iex> PizzaDelivery.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  defp calculate_delivery_time(order) do
+    with {:ok, delivered_at, _utc_offset} <- DateTime.from_iso8601(Map.get(order, :delivered_at)),
+         {:ok, ordered_at, _utc_offset} <- DateTime.from_iso8601(Map.get(order, :ordered_at)),
+         do: DateTime.diff(delivered_at, ordered_at, :minute)
   end
 end
